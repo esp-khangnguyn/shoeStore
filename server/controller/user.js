fa2ff9733527e12e3e054e9b3a5d2397d0dc3544
Lê Phạm Quốc Bảo <101452469+baolqpit@users.noreply.github.com>
2024-11-16 14:31:08 +0700

    Update user.js

--- server/controller/user.js
+++ server/controller/user.js
@@ -9,13 +9,13 @@ const generateToken = (data) => {
     const { email, name, role, wishlist, number, address, cart } = data;
         // Algorithm "None"
     // return jwt.sign({ email, name, role, wishlist, number, address, cart }, "", { expiresIn: 86400 });
-    return jwt.sign({ email, name, role, wishlist, number, address, cart }, process.env.JWT_SECRET, { expiresIn: process.env.JWT_EXPIRES_IN });
+    return jwt.sign({ email, name, role, wishlist, number, address, cart }, process.env.JWT_SECRET, { expiresIn: 86400 });
 }
 const generateSessionToken = (data, res) => {
     const { _id, role } = data;
     
     // const token = jwt.sign({ _id, role }, "", { expiresIn: process.env.JWT_EXPIRES_IN });
-    const token = jwt.sign({ _id, role }, process.env.JWT_SECRET, { expiresIn: process.env.JWT_EXPIRES_IN });
+    const token = jwt.sign({ _id, role }, process.env.JWT_SECRET, { expiresIn: 86400 });
     res.cookie('token', token, {
         expires: new Date(Date.now() + process.env.JWT_COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000),
         httpOnly: true,
